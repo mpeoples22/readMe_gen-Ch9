9431cf619d50339ccb4b0916716b70a4b6932fcb
mpeoples22 <mpeoples2020@gmail.com>
2022-01-09 14:01:07 -0600

    project setup

---
+++ Utilities/src/pageTemplate.js
@@ -0,0 +1,7 @@
+//function to generate page
+module.exports = tempData => {
+    //destructure selection
+    const {a, b, ...rest} = tempData;
+//page needs updating to suit readme project...
+  
+};
